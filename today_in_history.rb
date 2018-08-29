8d686417bd1e25a00f4dfe6591fe6a69dc315f12
Fábio Costa <fabioatc@gmail.com>
2018-08-29 18:04:04 +0100

    Random event type between: events, deaths, births. Rubocop applied.

--- today_in_history.rb
+++ today_in_history.rb
@@ -1,17 +1,23 @@
+# frozen_string_literal: true
+
 require 'net/https'
 require 'yaml'
+require 'json'
 
-API_CONFIG = YAML.load_file("config/api.yml")
+API_CONFIG = YAML.load_file('config/api.yml')
 
-uri = URI.parse(API_CONFIG["url"])
+uri = URI.parse(API_CONFIG['url'])
 
 # http get
 http = Net::HTTP.new(uri.host)
 request = Net::HTTP::Get.new(uri.request_uri)
 response = http.request(request)
 
-body = eval response.body
+body = JSON.parse(response.body)
 
-puts body[:date]
-puts "=" * 10
-body[:data][:Events].each do |event| puts "#{event[:year]} - #{event[:text]}" end
+types = %w[Events Births Deaths]
+type = types[Random.rand(types.size)]
+
+puts "#{body['date']} - #{type}"
+puts '=' * 10
+body['data'][type].each { |event| puts "#{event['year']} - #{event['text']}" }
